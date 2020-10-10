7c1f0ab770b89069f3db8ef9731a094237a63af1
sarangyang <sarang628@naver.com>
2020-10-10 14:03:41 +0900

    update

--- app/src/main/java/com/example/teleponymanager/MainActivity.kt
+++ app/src/main/java/com/example/teleponymanager/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.teleponymanager
 
+import android.Manifest
+import android.R
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telephony.TelephonyManager
 import android.util.Log
+import android.widget.ArrayAdapter
+import androidx.core.app.ActivityCompat
 import com.example.teleponymanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,17 +19,40 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(activityMainBinding.root)
 
-        val telephonyManager : TelephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            //Log.e("sarang", telephonyManager.imei)
-        }
+        val telephonyManager: TelephonyManager =
+            getSystemService(TELEPHONY_SERVICE) as TelephonyManager
 
         activityMainBinding.callState.text = telephonyManager.callState.toString()
 
-        activityMainBinding.btnRefresh.setOnClickListener {
-            activityMainBinding.callState.text = telephonyManager.callState.toString()
+        requestPermissions(arrayOf(Manifest.permission.READ_PHONE_STATE), 0x01)
+
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            activityMainBinding.imei.text = "${telephonyManager.imei}"
+        } else {
+            activityMainBinding.imei.text = "${telephonyManager.deviceId}"
         }
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_SMS
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_PHONE_NUMBERS
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_PHONE_STATE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            activityMainBinding.btnPermission.setOnClickListener {
+                requestPermissions(arrayOf(Manifest.permission.READ_SMS,
+                    Manifest.permission.READ_PHONE_NUMBERS,
+                    Manifest.permission.READ_PHONE_STATE), 0x01)
+            }
+            return
+        }
+        activityMainBinding.line1Number.text = "${telephonyManager.line1Number}"
+
 
     }
 }
